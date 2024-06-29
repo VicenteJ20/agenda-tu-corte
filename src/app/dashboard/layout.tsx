8b504c91493d0f3c98ef8aa56bbab14de0110084
Vicente Jorquera <jorquerav07@gmail.com>
2024-06-29 01:53:41 -0400

    feat/se agregan modificaiones visuales

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import { auth } from "@/auth";
 import { SessionProvider } from "next-auth/react";
 import { DashboardNavbar } from "@/components/dashboard/Navbar";
-
+import { HeaderBarTitle } from "@/components/dashboard/HeaderBarTitle";
 
 export default async function DashboardLayout({
   children,
@@ -11,10 +11,13 @@ export default async function DashboardLayout({
   const session = await auth()
   return (
     <SessionProvider session={session}>
-      <main className='flex flex-row bg-zinc-100'>
+      <main className='flex flex-row bg-zinc-100 relative'>
         <DashboardNavbar />
-        <section className='p-6 w-full transitions-all duration-300 ease-in-out'>
-          {children}
+        <section className='w-full transitions-all duration-300 ease-in-out'>
+          <HeaderBarTitle />
+          <section className='p-6 w-full'>
+            {children}
+          </section>
         </section>
       </main>
     </SessionProvider>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import { auth } from '@/auth'
-import { SignOutBtn } from '@/components/SignOutBtn'
 import { BarChartHome } from '@/components/dashboard/BarChartHome'
 import { SectionHeader } from '@/components/dashboard/SectionHeader'
 import { StatusCards } from '@/components/dashboard/StatusCards'
@@ -9,7 +8,7 @@ const DashboardAdminPage = async () => {
   const session = await auth()
   const { stats, graphicStats } = msj.DashboardHomePage as any
   return (
-    <section className='w-full relative h-[94vh] max-h-screen flex flex-col gap-10 justify-between'>
+    <section className='w-full h-[91vh] max-h-screen flex flex-col gap-10 justify-between'>
       <div>
         <SectionHeader title={stats.title} />
         <StatusCards />
